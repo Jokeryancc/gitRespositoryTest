c400ef1c36337f1921c3de7b1d914a416a388e02
jokeryan <jokeryan.test.com>
2020-09-06 21:31:54 +0800

    add test01.py

---
+++ test01.py
@@ -0,0 +1,24 @@
+# message_test="hello world!!!"
+# string01="this is a string"
+# string02 ='THis Is STring02'
+# string03='THIS IS A TESTSTRINg'
+# string04='this is a teststrinG04'
+# string05='test_string'
+# string06='string_test'
+
+# print(message_test)
+# print('hello world')
+# print(string01.title())
+# print(string01.upper())
+# print(string02.lower())
+# print(string03.lower())
+# print(string04.title())
+# print(string05+"---拼接--\t\t"+string06)
+# print(string05+"---拼接--\n"+string06)
+# string07='python       '
+# string08=' python'
+# print(string07.rsplit())
+# print(string08.rsplit())
+string10='       teststring       '
+print(string10.split())
+print(string10)
