abf9d859fc9c7d69af5a64b7c6a5372cab90b9df
root <root@localhost.localdomain>
2020-09-09 03:49:19 +0800

    add test02.py

---
+++ test02.py
@@ -0,0 +1,25 @@
+# encoding: utf-8
+'''
+author:joker
+'''
+cars = ['bwm','subarh','toyota' ]
+cars.reverse()
+print(cars)
+cars.reverse()
+print(cars)
+print(len(cars))
+print(cars.__len__())
+
+place = ['greatwall','sanya','huangshan','huashan','taishan']
+print(sorted(place))
+place.reverse()
+print(place)
+
+place.sort()
+print(place)
+
+place.sort()
+print(place)
+
+print(place[-1])
+print(len(place))
